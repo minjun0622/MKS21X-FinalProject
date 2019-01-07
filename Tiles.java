49718d3d9fa6c0389d12e9790ad1b61a46ada2ff
clee010922 <clee01@stuy.edu>
2019-01-07 10:11:13 -0500

    worked on Tiles class in class

---
+++ Tiles.java
@@ -0,0 +1,16 @@
+public class Tiles extends Board {
+
+  boolean isBomb, isNumber, isFlag, isClcked;
+  int row, col;
+
+  public Tiles(int rowVal, int colVal) {
+    row = rowVal;
+    col = colVal;
+    isBomb = false;
+    isNumber = false;
+    isFlag = false;
+    isClicked = false;
+  }
+  public String toString {
+    if ()
+  }
