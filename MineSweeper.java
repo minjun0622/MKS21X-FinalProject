fb934b090fa4f16b2487e7e6bac4d35c6fd1df42
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-01-08 10:16:15 -0500

    More methods and bug fixes.

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,11 +1,29 @@
 public class MineSweeper {
+  int seed;
 
+  public String displayTime() {
+    while (running) {
+    long tEnd = System.currentTimeMillis();
+    long millis = tEnd - tStart;
+    putString(1,2,terminal,"Milliseconds since start of program: "+millis);
+    if(millis/1000 > lastSecond){
+      lastSecond = millis / 1000;
+      //one second has passed.
+      putString(1,3,terminal,"Seconds since start of program: "+lastSecond);
+  }
+}
 
-
-
+}
 
 public static void main(String[] args) {
-  MineSweeper MS = new MineSweeper (5, 5);
-  System.out.println(MS);
+  if (args.length == 0) {
+    System.out.println( )
+  }
+
 }
+
+
+
+
+
 }
