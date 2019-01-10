521826592a05924299135bd160d88c6dd3e2098f
clee010922 <clee01@stuy.edu>
2019-01-10 09:38:30 -0500

    pasted code from yesterday's work because I couldn't commit yesterday due to branching issues

--- Tiles.java
+++ Tiles.java
@@ -1,7 +1,7 @@
-public class Tiles extends Board{
+public class Tiles { //extends Board ??
 
-  boolean isBomb, isNumber, isFlag, isClicked;
-  int row, col;
+  private boolean isBomb, isNumber, isFlag, isClicked;
+  private int row, col, numNeighborMines;
 
   public Tiles(int rowVal, int colVal) {
     row = rowVal;
@@ -11,6 +11,9 @@ public class Tiles extends Board{
     isFlag = false;
     isClicked = false;
   }
+  /*
+
+  needs to be worked on/is toString for Tiles needed?
 
   public String toString() {
     String result = "";
@@ -19,9 +22,40 @@ public class Tiles extends Board{
     if (isBomb && isClicked)
       result += "B";
     return result;
+
+  }*/
+
+  public int getNumNeighborMines() {
+    return numNeighborMines;
   }
 
+  public void setNumNeighborMines(int value) {
+    numNeighborMines = value;
+  }
 
+  public boolean isBomb() {
+    return isBomb;
+  }
+
+  public boolean isNumber() {
+    return isNumber;
+  }
+
+  public boolean isFlag() {
+    return isFlag;
+  }
+
+  public boolean isClicked() {
+    return isClicked;
+  }
+
+  public int row() {
+    return row;
+  }
+
+  public int col() {
+    return col;
+  }
 
 
 
